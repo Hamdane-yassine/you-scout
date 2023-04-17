64ac4fdf649f3ff32764b18f8048f1fcf9e4061b
Ayoubon <aarrasseayoub@gmail.com>
2023-04-17 22:26:08 +0000

    feed service without auth

--- user-feed-service/src/main/java/ma/ac/inpt/InstaFeedServiceApplication.java
+++ user-feed-service/src/main/java/ma/ac/inpt/InstaFeedServiceApplication.java
@@ -1,24 +1,35 @@
 package ma.ac.inpt;
 
+//import ma.ac.inpt.connection.datastax_astra_configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.file.Path;
+
 @SpringBootApplication
 //@EnableBinding(PostEventStream.class)
 @EnableFeignClients
+//@EnableCassandraRepositories
 public class InstaFeedServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InstaFeedServiceApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
 }
+//    @Bean
+//    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(datastax_astra_configuration astraProperties) {
+//        Path bundle = astraProperties.getSecureConnectionBundle().toPath();
+//        return builder -> builder.withCloudSecureConnectBundle(bundle);
+//    }
+//}
--- user-feed-service/src/main/java/ma/ac/inpt/repo/Cassandra.java
+++ user-feed-service/src/main/java/ma/ac/inpt/repo/Cassandra.java
@@ -3,6 +3,7 @@ package ma.ac.inpt.repo;
 import ma.ac.inpt.UserFeedEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.data.repository.CrudRepository;
 
 
