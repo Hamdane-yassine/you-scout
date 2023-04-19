82107a886a817bace629cc2b03ae88dd9cfd1e93
oussamaZAAM <zaam.oussama@gmail.com>
2023-04-19 17:26:21 +0000

    Add Skills Service

---
+++ skills-service/src/main/java/ma/ac/inpt/SkillsServiceApplication.java
@@ -0,0 +1,13 @@
+package ma.ac.inpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsServiceApplication.class, args);
+	}
+
+}
