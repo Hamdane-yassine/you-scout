3eaf9eb1ca18601e21b7395f89e8f620926a8998
oussamaZAAM <zaam.oussama@gmail.com>
2023-04-20 15:51:02 +0000

    Delete the deleted comment's id from the post's comments List + Exceptions improving

--- comments-service/src/main/java/ma/ac/inpt/exceptions/CommentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ma.ac.inpt.exceptions;
-
-public class CommentNotFoundException extends RuntimeException {
-    public CommentNotFoundException(String message) {
-        super(message);
-    }
-}
--- comments-service/src/main/java/ma/ac/inpt/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ma.ac.inpt.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-        System.out.println(message);
-    }
-}
