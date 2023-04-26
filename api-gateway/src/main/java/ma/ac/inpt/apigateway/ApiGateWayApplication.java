7139db19a66f919abbb52298080b22d55c02ba64
Yassine Hamdane <hamdaneyassine10@gmail.com>
2023-04-27 00:07:32 +0100

    update pull request

--- api-gateway/src/main/java/ma/ac/inpt/apigateway/ApiGateWayApplication.java
+++ api-gateway/src/main/java/ma/ac/inpt/apigateway/ApiGateWayApplication.java
@@ -1,11 +1,15 @@
 package ma.ac.inpt.apigateway;
 
+import ma.ac.inpt.apigateway.config.RsaKeysConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableConfigurationProperties(RsaKeysConfig.class)
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(exclude = PropertyPlaceholderAutoConfiguration.class)
 public class ApiGateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGateWayApplication.class);
