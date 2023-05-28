39f18a8c9cd1909e09866e862556af17bec26148
Yassine Hamdane <hamdaneyassine10@gmail.com>
2023-05-28 20:19:48 +0100

    Add UserMapper unit tests

--- auth-service/src/main/java/ma/ac/inpt/authservice/dto/ProfilePictureUpdateResponse.java
+++ auth-service/src/main/java/ma/ac/inpt/authservice/dto/ProfilePictureUpdateResponse.java
@@ -1,12 +1,17 @@
 package ma.ac.inpt.authservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ProfilePictureUpdateResponse {
     private String username;
     private String profilePictureUrl;
 }
+
 
