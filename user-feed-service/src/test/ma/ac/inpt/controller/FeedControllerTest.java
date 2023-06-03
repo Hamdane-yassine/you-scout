b958b88c0513e3bbf8cd4cedc136946f5e519437
Ayoubon <aarrasseayoub@gmail.com>
2023-06-03 12:37:01 +0100

    controller test

--- user-feed-service/src/test/ma/ac/inpt/controller/FeedControllerTest.java
+++ user-feed-service/src/test/ma/ac/inpt/controller/FeedControllerTest.java
@@ -46,4 +46,20 @@ public class FeedControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(expectedJson)); // replace with expected JSON response
     }
+@Test
+    public void testGetFeedWithPagingState() throws Exception {
+        String username = "testuser";
+        Optional<String> pagingState = Optional.of("W2gsf!");
+        SlicedResult<Post> feedResult = SlicedResult.<Post>builder().content(List.of(Post.builder().id("the one").build())).build();
+        System.out.println(feedResult.toString());
+        when(feedService.getUserFeed(username, pagingState)).thenReturn(feedResult);
+        String expectedJson = "{\"pagingState\":null,\"content\":[{\"id\":\"the one\",\"createdAt\":null,\"username\":null,\"userProfilePic\":null,\"updatedAt\":null,\"lastModifiedBy\":null,\"imageUrl\":null,\"caption\":null,\"commentsNum\":0}],\"last\":false}";
+
+        // Act and Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/feed/{username}", username)
+                        .param("ps", pagingState.orElse(""))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(expectedJson)); // replace with expected JSON response
+    }
 }
