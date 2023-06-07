22a099155d117cfd0ba578e798adafa1535a0577
Yassine Hamdane <hamdaneyassine10@gmail.com>
2023-06-07 16:15:16 +0100

    add k8s objects for deployment

--- api-gateway/src/main/java/ma/ac/inpt/apigateway/ApiGateWayApplication.java
+++ api-gateway/src/main/java/ma/ac/inpt/apigateway/ApiGateWayApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigurationProperties(RsaKeysConfig.class)
-@EnableEurekaClient
 @SpringBootApplication(exclude = PropertyPlaceholderAutoConfiguration.class)
 public class ApiGateWayApplication {
     public static void main(String[] args) {
