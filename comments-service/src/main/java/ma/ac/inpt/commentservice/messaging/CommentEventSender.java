cb8a260e1fa53db2fc33c8cd8ccce3d07d3d43e2
Yassine Hamdane <hamdaneyassine10@gmail.com>
2023-06-08 17:42:28 +0100

    update k8s manifests

--- comments-service/src/main/java/ma/ac/inpt/commentservice/messaging/CommentEventSender.java
+++ comments-service/src/main/java/ma/ac/inpt/commentservice/messaging/CommentEventSender.java
@@ -4,6 +4,7 @@ package ma.ac.inpt.commentservice.messaging;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ma.ac.inpt.commentservice.payload.CommentNumEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +15,13 @@ import org.springframework.stereotype.Service;
 public class CommentEventSender {
 
     private final KafkaTemplate<String, CommentNumEvent> kafkaTemplate;
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
 
     public void sendCommentNum(String postId, int commentsNum) {
         log.info("sending comment number event for post id {}", postId);
 
         CommentNumEvent commentNumEvent = new CommentNumEvent(postId, commentsNum);
-        kafkaTemplate.send("comment", commentNumEvent);
+        kafkaTemplate.send(topicName, commentNumEvent);
     }
 }
--- comments-service/src/main/java/ma/ac/inpt/commentservice/messaging/KafkaTopicConfig.java
+++ comments-service/src/main/java/ma/ac/inpt/commentservice/messaging/KafkaTopicConfig.java
@@ -1,15 +1,27 @@
 
 package ma.ac.inpt.commentservice.messaging;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 import org.apache.kafka.clients.admin.NewTopic;
 @Configuration
 public class KafkaTopicConfig {
+    /**
+     * The name of the Kafka topic.
+     */
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    /**
+     * Creates a new instance of the Kafka topic.
+     *
+     * @return the Kafka topic instance
+     */
     @Bean
-    public NewTopic commentTopic(){
-        return TopicBuilder.name("comment")
-                .build();
+    public NewTopic usersTopic() {
+        return TopicBuilder.name(topicName).build();
     }
-    }
+
+}
